004143e15bd361b379b3bd23e414295718329d41
Avinash9369 <avinashrairai540@gmail.com>
2024-07-18 22:52:41 +0530

    Done

---
+++ Sprint 1/Day2/basic-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Welcome to React Application</h1>
+     <img src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg"/>
+
+    </div>
+  );
+}
+
+export default App;
