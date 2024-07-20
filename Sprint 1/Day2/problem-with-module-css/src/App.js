87b128b35794847546ebe9c777ed51a283909623
Avinash9369 <avinashrairai540@gmail.com>
2024-07-20 12:58:30 +0530

    Done

---
+++ Sprint 1/Day2/problem-with-module-css/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="heading"> This app contains different components like MobileInfo, Courses, Users.</h1>
+    </div>
+  );
+}
+
+export default App;
